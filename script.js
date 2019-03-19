320c3a2f02c6f9258d9c7713e8c2c5c55a3c87d3
Vovka95 <volodimir.potskiy@gmailcom>
2019-03-19 19:13:20 +0200

    addmodal window for create elements, add style for folder/file, modal

--- script.js
+++ script.js
@@ -1,26 +1,79 @@
-const crElement = (isFolder) => {
-    console.log('createElement');
-    let div = document.createElement('div');
-    let img = document.createElement('img');
+const db = {
 
-    img.src = createImgItem(isFolder);
-
-    div.classList.add(isFolder ? 'data-folder' : 'data-file');
-    div.appendChild(img);
-
-    addElement(div);
 }
 
-const addElement = (element) => {
-    console.log('addElement');
-    let fileList = document.getElementById('files-list');
+const ui = {
+    crElement: (isFolder, itemName) => {
+        let div = document.createElement('div');
+        let hintDiv = document.createElement('div');
+        let p = document.createElement('p');
 
-    fileList.appendChild(element);
+        let titleText = document.createTextNode(itemName);
+        let hintText = document.createTextNode(itemName)
+    
+        div.appendChild(ui.createImgItem(isFolder));
+        p.appendChild(titleText);
+        div.appendChild(p);
+        hintDiv.appendChild(hintText);
+        div.appendChild(hintDiv);
 
-    return fileList;
+        div.id = itemName;
+        div.classList.add(isFolder ? 'data-folder' : 'data-file');
+    
+        ui.addElement(div);
+    },
+
+    addElement: (element) => {
+        let fileList = document.getElementById('files-list');
+    
+        fileList.appendChild(element);
+    
+        return fileList;
+    },
+
+    createImgItem: (isFolder) => {
+        let img = document.createElement('img');
+        img.src = isFolder ? './images/folder-solid.svg' : './images/file-solid.svg';
+        return img;
+    },
+
+    openCreateModal: (id, elType) => {
+        let div = document.getElementById(id);
+        let p = document.getElementById('descriptionPlaceholder');
+    
+        p.innerHTML = `Enter name of ${elType}:`;
+    
+        div.style.display = 'block';
+    },
+
+    closeCreateModal: (id) => {
+        document.getElementById(id).style.display = 'none';
+    },
+
+    getInputValue: (sId) => {
+		let inputValue = document.getElementById(sId).value;
+		document.getElementById(sId).value = '';
+		return inputValue;
+	},
+
+    getIsFolder: () => {
+        let elType = document.getElementById('descriptionPlaceholder');
+
+        return (elType.innerText.indexOf('folder') > 0) ? true : false;   
+    }
 }
 
-const createImgItem = (isFolder) => {
-    console.log('choose img');
-    return isFolder ? './images/folder-solid.svg' : './images/file-solid.svg';
+const handler = {
+    openCreateModal: (id, elType) => {
+        ui.openCreateModal(id, elType);
+    },
+
+    closeCreateModal: (id) => {
+        ui.closeCreateModal(id);
+    },
+
+    crElement: (id) => {
+        ui.crElement(ui.getIsFolder(), ui.getInputValue('itemName'));
+        ui.closeCreateModal(id);
+    }
 }
